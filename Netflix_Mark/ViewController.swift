4a436d736c86d69fbd62f7429f18eb947359cda4
MarkYu <kevin60445@gmail.com>
2022-02-22 21:28:59 +0800

    Initial Commit

---
+++ Netflix_Mark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Mark
+//
+//  Created by EMCT on 2022/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
