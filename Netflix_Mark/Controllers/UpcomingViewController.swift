b3a3a529a2b10b934fc7c9b037c3c9c456e70be5
MarkYu <kevin60445@gmail.com>
2022-02-25 23:57:33 +0800

    2022/2/25 - 建立 Upcoming TableView 裡面的Table (將fetch的title放入table)

--- Netflix_Mark/Controllers/UpcomingViewController.swift
+++ Netflix_Mark/Controllers/UpcomingViewController.swift
@@ -9,12 +9,61 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    private var titles: [TrendingTitleResponse.Title] = [TrendingTitleResponse.Title]()
+    
+    private let upcomingTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        //大標題
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming(){
+        APICaller.shared.getUpcomingMovies { [weak self]result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                //更新畫面必須在主執行緒
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 
 
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
+        return cell
+    }
 }
